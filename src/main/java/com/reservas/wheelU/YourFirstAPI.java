51066f82ec07cd73f2f48140687b35a6c29122f2
Andrés Fabián Ramos benavides <andresfabi90@gmail.com>
2018-11-20 21:36:44 -0500

    Cambio en exepciones

--- src/main/java/com/reservas/wheelU/YourFirstAPI.java
+++ src/main/java/com/reservas/wheelU/YourFirstAPI.java
@@ -2,10 +2,13 @@ package com.reservas.wheelU;
 
 import java.util.ArrayList;
 
+import com.google.api.server.spi.ServiceException;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.ForbiddenException;
+import com.google.api.server.spi.response.NotFoundException;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.reservas.entidades.Reserva;
 import com.reservas.entidades.Usuario;
@@ -23,12 +26,12 @@ public class YourFirstAPI {
 	Facade facade = Facade.getInstance();
 	Proxy proxy = Proxy.getInstance();
 
-	@ApiMethod(name = "obtenerReservas")
+	@ApiMethod(name = "obtenerReservas", httpMethod = ApiMethod.HttpMethod.GET)
 	public ArrayList<Reserva> reservas() {
 		return facade.getReservas();
 	}
 
-	@ApiMethod(name = "obtenerUsuarios")
+	@ApiMethod(name = "obtenerUsuarios", httpMethod = ApiMethod.HttpMethod.GET)
 	public ArrayList<Usuario> obtenerUsuarios() {
 		return facade.getUsuarios();
 	}
@@ -44,39 +47,42 @@ public class YourFirstAPI {
 	}
 
 	@ApiMethod(name = "iniciarSesion")
-	public Aleatorio login(@Named("Correo") String correo, @Named("Password") String password) throws UnauthorizedException {
+	public Aleatorio login(@Named("correo") String correo, @Named("password") String password)
+			throws ForbiddenException {
 		Aleatorio aleatorio = proxy.iniciarSesion(correo, password);
-		if (aleatorio.getCorreo() == null) {
-			throw new UnauthorizedException("Invalid credentials");
+		if (aleatorio.getAleatorio() == 0) {
+			throw new ForbiddenException("Invalid credentials");
 		}
 		Proxy proxy = Proxy.getInstance();
 		return proxy.iniciarSesion(correo, password);
 	}
 
 	@ApiMethod(name = "ModificarReserva")
-	public Reserva modificarReserva(@Named("NombreReserva") String nombreReserva, @Named("IDRutaReservada") int IDRutaReservada, 
-			@Named("DocumentoPasajero") String documentoPasajero, Aleatorio aleatorio) throws UnauthorizedException {
+	public Reserva modificarReserva(@Named("nombreReserva") String nombreReserva,
+			@Named("IDRutaReservada") int IDRutaReservada, @Named("documentoPasajero") String documentoPasajero,
+			Aleatorio aleatorio) throws ServiceException {
 		if (!facade.isSesion(aleatorio, documentoPasajero)) {
-			throw new UnauthorizedException("Invalid credentials");
+			throw new ForbiddenException("Invalid credentials");
 		}
-		
+
 		Reserva reservaModificada = facade.modificarReserva(nombreReserva, IDRutaReservada, documentoPasajero);
-		
+
 		if (reservaModificada == null) {
-			throw new UnauthorizedException("Reserva no encontrada");
+			throw new NotFoundException("Reserva no encontrada");
 		}
-		
+
 		return reservaModificada;
 	}
-	
+
 	@ApiMethod(name = "ConsultarReservas")
-	public ArrayList<Reserva> consultarReservas(@Named("DocumentoPasajero") String documentoPasajero, Aleatorio aleatorio) throws UnauthorizedException {
+	public ArrayList<Reserva> consultarReservas(@Named("documentoPasajero") String documentoPasajero,
+			Aleatorio aleatorio) throws ServiceException {
 		if (!facade.isSesion(aleatorio, documentoPasajero)) {
-			throw new UnauthorizedException("Invalid credentials");
+			throw new ForbiddenException("Invalid credentials");
 		}
 		ArrayList<Reserva> reservasPasajero = facade.consultarReservas(documentoPasajero);
 		if (reservasPasajero.isEmpty()) {
-			throw new UnauthorizedException("Reservas no encontradas");
+			throw new NotFoundException("Reservas no encontradas");
 		}
 		return reservasPasajero;
 	}
